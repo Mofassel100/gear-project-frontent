237951e3b49a6dc16bdcd0dc2b271531d6c18502
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2026-08-11 00:19:00 +0600

    I hace setup gear project and login created

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+
+import { Navbar } from "@/components/shared/navbar";
+import { getMe } from "@/service/getMe";
+
+const AuthGroupLayout = async (
+    { children }: { children: React.ReactNode }) => {
+
+         const user = await getMe();
+         console.log(user.data.name)
+  return <div>
+    <Navbar user={user}/>
+    {children}
+  
+  </div>;
+};
+
+export default AuthGroupLayout;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="text-red-800 justify-center">My name is sifat</div>
+  );
+}
